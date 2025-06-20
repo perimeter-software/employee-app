d043ddf90700fc0fbc22f905ea1879e69098cb98
Haris Khan <kharis776@gmail.com>
2025-06-20 21:04:33 +0500

    Module resolution fixed

--- next.config.ts
+++ next.config.ts
@@ -50,8 +50,11 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-    webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
     return config;
   },
 };
